7036c7beaf08191ed2a179343e54ddb122ab0d5e
David Négrier <d.negrier@thecodingmachine.com>
2019-02-22 11:34:08 +0100

    Switching Xdebug version to newer 2.7.0RC2 for PHP 7.3

--- extensions/7.3/xdebug/install.sh
+++ extensions/7.3/xdebug/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-PECL_EXTENSION=xdebug-2.7.0beta1 PHP_EXT_NAME=xdebug DEPENDENCIES="bind9-host" ../docker-install.sh
+PECL_EXTENSION=xdebug-2.7.0RC2 PHP_EXT_NAME=xdebug DEPENDENCIES="bind9-host" ../docker-install.sh
