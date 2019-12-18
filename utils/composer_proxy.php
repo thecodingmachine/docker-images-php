0f65e0a93b421ba5d137bf54b771c967c36c10d8
David Négrier <d.negrier@thecodingmachine.com>
2019-12-18 22:50:38 +0100

    Adding comments

--- utils/composer_proxy.php
+++ utils/composer_proxy.php
@@ -1,6 +1,7 @@
 #!/usr/bin/env php
 <?php
 
+// We regenerate the configuration from environment variable, but only if the container is not started (i.e. if we are in a BUILD stage)
 if (!file_exists('/opt/container_started')) {
     passthru('php /usr/local/bin/generate_conf.php | sudo tee /etc/php/'.PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION.'/mods-available/generated_conf.ini > /dev/null');
     passthru('php /usr/local/bin/setup_extensions.php | sudo bash');
@@ -10,4 +11,5 @@ array_shift($argv);
 
 $args = array_map(function(string $item) { return escapeshellarg($item); }, $argv);
 
+// Let's pass the command down to the real composer
 passthru('real_composer '.implode(' ', $args));
