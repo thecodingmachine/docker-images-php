5432a01e527d0bb09ca50fc289fd1575ad46e594
Julien Neuhart <j.neuhart@thecodingmachine.com>
2020-12-07 11:51:40 +0100

    Add PHP script for checking PHP_* env var changes

---
+++ utils/check_php_env_var_changes.php
@@ -0,0 +1,39 @@
+<?php
+
+include '/opt/php_env_var_cache.php';
+
+$envVars = getenv();
+$shouldGenerateConfig = false;
+$phpEnvVar = [];
+
+foreach($envVars as $key => $value) {
+    if (substr($key, 0, strlen('PHP_')) === 'PHP_') {
+        if (!isset($phpEnvVarCache[$key])) {
+            // The env var does not exist in the cache.
+            $shouldGenerateConfig = true;
+        } else if ($phpEnvVarCache[$key] !== $value) {
+            // The value has changed.
+            $shouldGenerateConfig = true;
+        }
+
+        $phpEnvVar[$key] = $value;
+    }
+}
+
+if ($shouldGenerateConfig === false) {
+    echo "0";
+    exit(0);
+}
+
+$cacheFileContent = '<?php' . PHP_EOL . '$phpEnvVarCache = [];' . PHP_EOL;
+foreach ($phpEnvVar as $key => $value) {
+    $cacheFileContent .= '$phpEnvCache["' . $key . '"] = "' . $value . '";' . PHP_EOL;
+}
+
+$result = file_put_contents('/opt/php_env_var_cache.php', $cacheFileContent);
+if ($result === false) {
+    exit(1);
+}
+
+echo "1";
+exit(0);
--- utils/php_proxy.sh
+++ utils/php_proxy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# We regenerate the configuration from environment variable, but only if the container is not started (i.e. if we are in a BUILD stage or if overloading the entrypoint)
-if [[ ! -f /opt/container_started  ]]; then
+REGENERATE=$(/usr/bin/real_php /usr/local/bin/check_php_env_var_changes.php)
+
+if [[ "$REGENERATE" == "1" ]]; then
   /usr/bin/real_php /usr/local/bin/generate_conf.php | sudo tee "/etc/php/${PHP_VERSION}/mods-available/generated_conf.ini" > /dev/null
   /usr/bin/real_php /usr/local/bin/setup_extensions.php | sudo bash
-  sudo touch /opt/container_started
 fi
 
 /usr/bin/real_php "$@"
