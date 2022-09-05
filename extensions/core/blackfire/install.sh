4c2e1157b6fe085d37ffc7770c695f4626f4fbcd
Mistral OZ - MIO <m.oz@thecodingmachine.com>
2022-09-05 13:01:52 +0200

    Fix installation of blackfire (more regular way)

--- extensions/core/blackfire/install.sh
+++ extensions/core/blackfire/install.sh
@@ -4,8 +4,6 @@ set -e
 set -ex
 
 # Install Blackfire
-version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;")
-
 if [[ -z "${BLACKFIRE_VERSION}" ]]; then
   echo "Blackfire version is not set in the environment variables. Exiting!"
   exit 1
@@ -14,36 +12,32 @@ fi
 # Let's make it flexible: for those who want to be safe, the image will be built with v1
 # Now if you build the image yourself, you can build it with v2, this way everyone gets happy :)
 
-mkdir /tmp/blackfire
+version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;")
+# Probe is the same for v1 and v2
+curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe/php/${TARGETOS}/${TARGETARCH}/$version
+mkdir -p /tmp/blackfire
+tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp/blackfire
+mv /tmp/blackfire/blackfire-*.so $(php -r "echo ini_get ('extension_dir');")/blackfire.so
+printf "extension=blackfire.so" > "/etc/php/${PHP_VERSION}/mods-available/blackfire.ini"
+rm -rf /tmp/blackfire /tmp/blackfire-probe.tar.gz
 
 if [ $BLACKFIRE_VERSION == "1" ]; then
-    echo "Installing Blackfire version 1"
-    # curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v2/releases/probe/php/linux/amd64/$version
-    curl -o /tmp/blackfire/blackfire.so "https://packages.blackfire.io/binaries/blackfire-php/1.78.0/blackfire-php-${TARGETOS}"_"${TARGETARCH}-php-${version}.so"
-    mv /tmp/blackfire/blackfire.so $(php -r "echo ini_get('extension_dir');")/blackfire.so
-    echo "extension=blackfire" > /etc/php/${PHP_VERSION}/mods-available/blackfire.ini
-
-    # Adding this in the list of Ubuntu extensions because we use that list as a base for the modules list.
-    # TODO: question: cannot we use /etc/php/mods-available instead???
-    touch /var/lib/php/modules/${PHP_VERSION}/registry/blackfire
-    # curl -A "Docker" -L https://blackfire.io/api/v1/releases/client/linux_static/amd64 | tar zxp -C /tmp/blackfire
-    curl -o /tmp/blackfire/blackfire "https://packages.blackfire.io/binaries/blackfire-agent/1.50.0/blackfire-cli-${TARGETOS}"_"${TARGETARCH}"
-    chmod +x /tmp/blackfire/blackfire
-    mv /tmp/blackfire/blackfire /usr/bin/blackfire
-    rm -Rf /tmp/blackfire
-
+  # Cli for v1 require specific version
+  curl -A "Docker" "https://packages.blackfire.io/binaries/blackfire-agent/1.50.0/blackfire-cli-${TARGETOS}"_"${TARGETARCH}"  -o /tmp/blackfire
+  chmod +x /tmp/blackfire
+  mv /tmp/blackfire /usr/bin/blackfire
+  rm -Rf /tmp/blackfire
 elif [ $BLACKFIRE_VERSION == "2" ]; then
-    echo "Installing Blackfire version 2..."
-   
-    curl -o /tmp/blackfire/blackfire.so "https://packages.blackfire.io/binaries/blackfire-php/1.78.0/blackfire-php-${TARGETOS}"_"${TARGETARCH}-php-${version}.so"
-    mv /tmp/blackfire/blackfire.so $(php -r "echo ini_get('extension_dir');")/blackfire.so
-    echo "extension=blackfire.so" > /etc/php/${PHP_VERSION}/mods-available/blackfire.ini
-    touch /var/lib/php/modules/${PHP_VERSION}/registry/blackfire
-    curl -o /tmp/blackfire-cli.tar.gz "https://packages.blackfire.io/binaries/blackfire/2.10.0/blackfire-"${TARGETOS}"_"${TARGETARCH}".tar.gz"
-    tar zxpf /tmp/blackfire-cli.tar.gz -C /tmp/blackfire
-    mv /tmp/blackfire/blackfire /usr/bin/blackfire
-
-    rm -Rf /tmp/blackfire
+  # Cli for v2 is latest version
+  echo "Installing Blackfire version 2..."
+  mkdir -p /tmp/blackfire
+  curl -A "Docker" -L https://blackfire.io/api/v1/releases/cli/linux/${TARGETARCH} | tar zxp -C /tmp/blackfire
+  if ! /tmp/blackfire/blackfire self:version --no-ansi | grep -qE "version 2\.[0-9]+\.[0-9]+"; then
+    echo "Blackfire installed is not version 2 : $(/tmp/blackfire/blackfire self:version --no-ansi)"
+    exit 1
+  fi
+  mv /tmp/blackfire/blackfire /usr/bin/blackfire
+  rm -Rf /tmp/blackfire
 else
     echo "Blackfire version in environment variable is either empty or the value is invalid"
     echo "Value: '$BLACKFIRE_VERSION'"
