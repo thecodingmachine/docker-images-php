0776fba8cc557a0e7ffcfbf929653be8846b8062
Mistral OZ - MIO <m.oz@thecodingmachine.com>
2022-09-19 15:49:20 +0200

    Force php-swoole version to 4.8.11 for php7.*

--- extensions/core/swoole/install.sh
+++ extensions/core/swoole/install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+
 set -e
-export DEV_DEPENDENCIES="zlib1g-dev"
-export DEPENDENCIES="zlib1g"
-export USE_PECL=1
-PECL_EXTENSION=swoole ../docker-install.sh
+if [[ "${PHP_VERSION}" ~= "^7" ]]; then
+  ## http://pecl.php.net/package/swoole
+  export DEPENDENCIES="zlib1g"
+  export DEV_DEPENDENCIES="zlib1g-dev"
+  export USE_PECL=1
+  export PECL_EXTENSION=swoole-4.8.11
+  export PHP_EXT_NAME=swoole
+else
+  export EXTENSION=swoole
+fi
+
+../docker-install.sh
