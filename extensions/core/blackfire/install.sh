b840fda04c539975a4d6b7a592d37931d0cd142e
Mistral OZ - MIO <m.oz@thecodingmachine.com>
2022-09-19 15:46:57 +0200

    Fix blackfire install

--- extensions/core/blackfire/install.sh
+++ extensions/core/blackfire/install.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-set -ex
 
 # Install Blackfire
-version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;")
-
-if [[ -z "${BLACKFIRE_VERSION}" ]]; then
+if [[ "${BLACKFIRE_VERSION}" == "" ]]; then
   echo "Blackfire version is not set in the environment variables. Exiting!"
   exit 1
 fi
@@ -14,40 +11,43 @@ fi
 # Let's make it flexible: for those who want to be safe, the image will be built with v1
 # Now if you build the image yourself, you can build it with v2, this way everyone gets happy :)
 
-mkdir /tmp/blackfire
+version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;")
+# Probe is the same for v1 and v2
+curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe/php/${TARGETOS}/${TARGETARCH}/$version
+mkdir -p /tmp/blackfire
+tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp/blackfire
+mv /tmp/blackfire/blackfire-*.so $(php -r "echo ini_get ('extension_dir');")/blackfire.so
+printf "extension=blackfire.so" > "/etc/php/${PHP_VERSION}/mods-available/blackfire.ini"
+rm -rf /tmp/blackfire /tmp/blackfire-probe.tar.gz
 
-# Todo: Maybe version the Blackfire version by environment variable? :)
-
-if [ $BLACKFIRE_VERSION == "1" ]; then
-    echo "Installing Blackfire version 1"
-    # curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v2/releases/probe/php/linux/amd64/$version
-    curl -o /tmp/blackfire/blackfire.so "https://packages.blackfire.io/binaries/blackfire-php/1.78.0/blackfire-php-$CONTAINER_OS"_"$CONTAINER_ARCH-php-$version.so"
-    mv /tmp/blackfire/blackfire.so $(php -r "echo ini_get('extension_dir');")/blackfire.so
-    echo "extension=blackfire.so" > /etc/php/${PHP_VERSION}/mods-available/blackfire.ini
-
-    # Adding this in the list of Ubuntu extensions because we use that list as a base for the modules list.
-    # TODO: question: cannot we use /etc/php/mods-available instead???
-    touch /var/lib/php/modules/${PHP_VERSION}/registry/blackfire
-    # curl -A "Docker" -L https://blackfire.io/api/v1/releases/client/linux_static/amd64 | tar zxp -C /tmp/blackfire
-    curl -o /tmp/blackfire/blackfire "https://packages.blackfire.io/binaries/blackfire-agent/1.50.0/blackfire-cli-$CONTAINER_OS"_"$CONTAINER_ARCH"
-    chmod +x /tmp/blackfire/blackfire
-    mv /tmp/blackfire/blackfire /usr/bin/blackfire
-    rm -Rf /tmp/blackfire
-
-elif [ $BLACKFIRE_VERSION == "2" ]; then
-    echo "Installing Blackfire version 2..."
-   
-    curl -o /tmp/blackfire/blackfire.so "https://packages.blackfire.io/binaries/blackfire-php/1.78.0/blackfire-php-$CONTAINER_OS"_"$CONTAINER_ARCH-php-$version.so"
-    mv /tmp/blackfire/blackfire.so $(php -r "echo ini_get('extension_dir');")/blackfire.so
-    echo "extension=blackfire.so" > /etc/php/${PHP_VERSION}/mods-available/blackfire.ini
-    touch /var/lib/php/modules/${PHP_VERSION}/registry/blackfire
-    curl -o /tmp/blackfire-cli.tar.gz "https://packages.blackfire.io/binaries/blackfire/2.10.0/blackfire-"$CONTAINER_OS"_"$CONTAINER_ARCH".tar.gz"
-    tar zxpf /tmp/blackfire-cli.tar.gz -C /tmp/blackfire
-    mv /tmp/blackfire/blackfire /usr/bin/blackfire
-
-    rm -Rf /tmp/blackfire
+if [[ "${BLACKFIRE_VERSION}" == "1" ]]; then
+  # Cli for v1 require specific version
+  curl -A "Docker" "https://packages.blackfire.io/binaries/blackfire-agent/1.50.0/blackfire-cli-${TARGETOS}"_"${TARGETARCH}"  -o /tmp/blackfire
+  chmod +x /tmp/blackfire
+  mv /tmp/blackfire /usr/bin/blackfire
+  rm -Rf /tmp/blackfire
+elif [[ "${BLACKFIRE_VERSION}" == "2" ]]; then
+  # Cli for v2 is latest version
+  echo "Installing Blackfire version 2..."
+  mkdir -p /tmp/blackfire
+  curl -A "Docker" -L https://blackfire.io/api/v1/releases/cli/linux/${TARGETARCH} | tar zxp -C /tmp/blackfire
+  if ! /tmp/blackfire/blackfire self:version --no-ansi | grep -qE "version 2\.[0-9]+\.[0-9]+"; then
+    echo "Blackfire installed is not version 2 : $(/tmp/blackfire/blackfire self:version --no-ansi)"
+    exit 1
+  fi
+  mv /tmp/blackfire/blackfire /usr/bin/blackfire
+  rm -Rf /tmp/blackfire
 else
-    echo "Blackfire version in environment variable is either empty or the value is invalid"
-    echo "Value: '$BLACKFIRE_VERSION'"
+    echo "Blackfire version in environment variable is invalid"
+    echo "Value: '${BLACKFIRE_VERSION}'"
     exit 1
 fi
+
+# Let's perform a test
+phpenmod -v $PHP_VERSION blackfire
+/usr/bin/real_php -m | grep "blackfire"
+# Check that there is no output on STDERR when starting php:
+OUTPUT=`/usr/bin/real_php -r "echo '';" 2>&1`
+[[ "$OUTPUT" == "" ]]
+# And now, let's disable it!
+phpdismod -v $PHP_VERSION blackfire
