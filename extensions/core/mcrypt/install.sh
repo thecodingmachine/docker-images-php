779e4407545bf13515116d2cfd5a41f78f247bfc
Mistral OZ - MIO <m.oz@thecodingmachine.com>
2022-09-19 15:42:01 +0200

    Move legacy mcrypt install to same core file

--- extensions/core/mcrypt/install.sh
+++ extensions/core/mcrypt/install.sh
@@ -3,10 +3,16 @@
 set -e
 # Comments: MCrypt is deprecated and usage is generally discouraged. Provided here for legacy apps only.
 
-export USE_PECL=1
-export PECL_EXTENSION=mcrypt-1.0.1
-export PHP_EXT_NAME=mcrypt # name of the extension (to put in PHP_EXTENSIONS variable)
-export DEV_DEPENDENCIES="libmcrypt-dev"
-export DEPENDENCIES="libmcrypt4"
+if false; then # legacy install
+  export USE_PECL=1
+  export PECL_EXTENSION=mcrypt-1.0.1
+  export PHP_EXT_NAME=mcrypt # name of the extension (to put in PHP_EXTENSIONS variable)
+  export DEV_DEPENDENCIES="libmcrypt-dev"
+  export DEPENDENCIES="libmcrypt4"
+else # Regular install
+  export EXTENSION=mcrypt
+  export DEV_DEPENDENCIES="libmcrypt-dev"
+  export DEPENDENCIES="libmcrypt4"
+fi
 
 ../docker-install.sh
